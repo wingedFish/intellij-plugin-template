ab521bc77bf183a31042c919745732c25dbae6a4
GitHub Action <action@github.com>
2022-03-15 06:36:30 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/wingedfish/intellijplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.wingedfish.intellijplugintemplate.services
+
+import com.github.wingedfish.intellijplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
